a410a5de88c0b497b7fa9e026580d070755a1b2a
Sadakatul Shakil <sadakatul.shakil@adndiginet.com>
2022-06-05 18:33:54 +0600

    one to one video calling run successfully

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,118 +1,146 @@
+import 'dart:async';
+
 import 'package:agora_rtc_engine/rtc_engine.dart';
-import 'package:agora_vedio_call/pages/call.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class IndexPage extends StatefulWidget{
+import './call.dart';
 
+class IndexPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => InitState();
-
+  State<StatefulWidget> createState() => IndexState();
 }
-class InitState extends State<IndexPage> {
 
+class IndexState extends State<IndexPage> {
+  /// create a channelController to retrieve text value
   final _channelController = TextEditingController();
-  bool _validateError  = false;
+
+  /// if channel textField is validated to have error
+  bool _validateError = false;
+
   ClientRole? _role = ClientRole.Broadcaster;
 
   @override
   void dispose() {
-    // TODO: implement dispose
+    // dispose input controller
     _channelController.dispose();
     super.dispose();
   }
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-  }
-  @override
   Widget build(BuildContext context) {
-    return initWidget(context);
-  }
-
-  Widget initWidget(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
-        backgroundColor: const Color(0xFF00A6FF),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
-        title: Text('Video Call Service',),
-        centerTitle: true,
+      appBar: AppBar(
+        title: Text('Agora Flutter QuickStart'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            const SizedBox(height: 40,),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: 400,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: TextField(
+                        controller: _channelController,
+                        decoration: InputDecoration(
+                          errorText:
+                          _validateError ? 'Channel name is mandatory' : null,
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(width: 1),
+                          ),
+                          hintText: 'Channel name',
+                        ),
+                      ))
+                ],
+              ),
+              Column(
+                children: [
+                  ListTile(
+                    title: Text(ClientRole.Broadcaster.toString()),
+                    leading: Radio(
+                      value: ClientRole.Broadcaster,
+                      groupValue: _role,
+                      onChanged: (ClientRole? value) {
+                        setState(() {
+                          _role = value;
+                        });
+                      },
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(ClientRole.Audience.toString()),
+                    leading: Radio(
+                      value: ClientRole.Audience,
+                      groupValue: _role,
+                      onChanged: (ClientRole? value) {
+                        setState(() {
+                          _role = value;
+                        });
+                      },
+                    ),
+                  )
+                ],
+              ),
               Padding(
-                padding: const EdgeInsets.only(left: 50.0),
-                child: Image.network('https://tinyurl.com/2p889y4k'),
-              ),
-            const SizedBox(height: 20,),
-            TextField(
-              controller: _channelController,
-              decoration: InputDecoration(
-                errorText: _validateError ? 'channel name is mandatory':null,
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(width: 1)
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: onJoin,
+                        child: Text('Join'),
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                            foregroundColor: MaterialStateProperty.all(Colors.white)
+                        ),
+                      ),
+                    ),
+                    // Expanded(
+                    //   child: RaisedButton(
+                    //     onPressed: onJoin,
+                    //     child: Text('Join'),
+                    //     color: Colors.blueAccent,
+                    //     textColor: Colors.white,
+                    //   ),
+                    // )
+                  ],
                 ),
-                hintText: 'Channel name'
-              ),
-            ),
-            RadioListTile(
-              title: Text('Broadcaster'),
-              onChanged: (ClientRole? value){
-                setState(() {
-                  _role = value;
-                });
-              },
-              value: ClientRole.Broadcaster,
-              groupValue: _role,
-            ),
-            RadioListTile(
-              title: Text('Audience'),
-              onChanged: (ClientRole? value){
-                setState(() {
-                  _role = value;
-                });
-              },
-              value: ClientRole.Audience,
-              groupValue: _role,
-            ),
-            ElevatedButton(
-                onPressed: onJoin,
-                child: Text('Join'),
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(double.infinity, 40)
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Future<void> onJoin() async{
+  Future<void> onJoin() async {
+    // update input validation
     setState(() {
-      _channelController.text.isEmpty?
-      _validateError = true:
-      _validateError = false;
+      _channelController.text.isEmpty
+          ? _validateError = true
+          : _validateError = false;
     });
-    if(_channelController.text.isNotEmpty) {
-      PermissionStatus microphone = await Permission.microphone.request();
-      PermissionStatus camera = await Permission.camera.request();
-      if (microphone == PermissionStatus.granted &&
-          camera == PermissionStatus.granted) {
-        await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) =>
-                CallPage(_channelController.text, _role)));
-      }
+    if (_channelController.text.isNotEmpty) {
+      // await for camera and mic permissions before pushing video page
+      await _handleCameraAndMic(Permission.camera);
+      await _handleCameraAndMic(Permission.microphone);
+      // push video page with given channel name
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CallPage(
+            channelName: _channelController.text,
+            role: _role,
+          ),
+        ),
+      );
     }
+  }
+
+  Future<void> _handleCameraAndMic(Permission permission) async {
+    final status = await permission.request();
+    print(status);
   }
 }
--- lib/utils/settings.dart
+++ lib/utils/settings.dart
@@ -1,3 +1,3 @@
-
-const appId = 'f8cf0c9ce74f4e5783d89b9db92c8706';
-const token = '006f8cf0c9ce74f4e5783d89b9db92c8706IABSMAaGqB+SRSegRNL2rTNr4tVbPbXNClsWqDnBFiY+Lx1zy08AAAAAEACXVkQu0QCdYgEAAQDQAJ1i';
+/// Define App ID and Token
+const APP_ID = 'f8cf0c9ce74f4e5783d89b9db92c8706';
+const Token = '006f8cf0c9ce74f4e5783d89b9db92c8706IACUJbr0OPF96laU9zEIsG0hoEq2bgFbultZVnc0RPTPzAx+f9gAAAAAEACXVkQu84udYgEAAQDyi51i';
